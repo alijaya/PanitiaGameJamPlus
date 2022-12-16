09b281c931c7be40ecd02ca2bb359e01db9e4ca6
myqpalzm <jonathan.franzeli@student.umn.ac.id>
2022-12-16 11:10:03 +0700

    added sprite outline generator button in inspector

--- Assets/_outline-test/OutlineMaker.cs
+++ Assets/_outline-test/OutlineMaker.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class OutlineMaker : MonoBehaviour
@@ -9,8 +11,19 @@ public class OutlineMaker : MonoBehaviour
     [SerializeField] private List<GameObject> sprites;
     [SerializeField] private Material material;
 
+    public List<GameObject> _outlines;
+
     private void Awake()
     {
+        if (outlineParent.childCount == 0) InstantiateOutlines();
+    }
+    
+    private void InstantiateOutlines()
+    {
+        if (_outlines.Count >= 1) return;
+        
+        DeleteOutlines();
+        
         foreach (GameObject sprite in sprites)
         {
             GameObject go = Instantiate(sprite, sprite.transform.position, sprite.transform.rotation, outlineParent);
@@ -22,6 +35,27 @@ public class OutlineMaker : MonoBehaviour
 
             Vector3 scale = go.transform.localScale;
             go.transform.localScale = new Vector3(scale.x + outlineThickness, scale.y + outlineThickness, scale.z);
+            
+            _outlines.Add(go);
         }
+    }
+    
+    [PropertySpace]
+
+    [Button("Generate Outlines")]
+    private void InstantiateOutlinesEditor()
+    {
+        InstantiateOutlines();
+    }
+
+    [Button("Delete Outlines")]
+    private void DeleteOutlines()
+    {
+        foreach (GameObject outline in _outlines)
+        {
+            DestroyImmediate(outline);    
+        }
+
+        _outlines.Clear();
     }
 }
