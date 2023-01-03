dfe776957afa72a0e98d4e2e825864546735d60e
Ali Jaya Meilio <alijayameilio@gmail.com>
2023-01-03 21:06:20 +0700

    bug fix cashier queue

--- Assets/Scripts/Util/AsyncQueue.cs
+++ Assets/Scripts/Util/AsyncQueue.cs
@@ -7,34 +7,11 @@ using System;
 
 public class AsyncQueue
 {
-
-    //private int queueHeadIndex = 0;
-    //private int queueTailIndex = 0;
-
-    //private AsyncLazy lastTask;
-
     private UniTask lastTask = UniTask.CompletedTask;
 
     // can not cancel ._.
-    public async UniTask Queue(Func<UniTask> task, CancellationToken ct = default)
+    public async UniTask Queue(Func<UniTask> task)
     {
-        //// get the waiting number
-        //var currentIndex = queueTailIndex++;
-
-        //// if queueHeadIndex is not currentIndex, it means it still processes another request
-        //var cancel = await UniTask.WaitUntil(() => queueHeadIndex == currentIndex, PlayerLoopTiming.Update, ct).SuppressCancellationThrow(); // wait until we process this request
-
-        //// actual stuff To Do
-        //if (!cancel) await task().SuppressCancellationThrow();
-        //queueHeadIndex++; // advance the process number
-
-        //if (queueHeadIndex == queueTailIndex)
-        //{
-        //    // if this is the last task, well reset it, so we don't get big number
-        //    queueHeadIndex = 0;
-        //    queueTailIndex = 0;
-        //}
-
         // push task
         var curTask = lastTask;
         var lastTaskSource = new UniTaskCompletionSource();
